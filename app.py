6036afb73089ebff87f29df7139c67676d068c46
rysamuel9 <ronysamuel19@gmail.com>
2021-11-26 21:14:44 +0700

    V1.1 Bug fixed

--- app.py
+++ app.py
@@ -37,22 +37,24 @@ gunting = '''
 gameIcon = [batu, kertas, gunting]
 
 userPilih = int(input("Pilihan user: "))
-print(gameIcon[userPilih])
-
-comPilih = random.randint(0, 2)
-print("Pilihan komputer: {}".format(comPilih))
-print(gameIcon[comPilih])
-
 
 if userPilih >= 3 or userPilih < 0:
     print("Invalid nomor, kamu kalah!")
-elif userPilih == 0 and comPilih == 2:
-    print("Kamu menang!")
-elif comPilih == 0 and userPilih == 2:
-    print("Kamu kalah!")
-elif comPilih > userPilih:
-    print("Kamu kalah!")
-elif userPilih > comPilih:
-    print("Kamu menang!")
-elif comPilih == comPilih:
-    print("Draw!")
+else:
+    print(gameIcon[userPilih])
+
+    comPilih = random.randint(0, 2)
+    print("Pilihan komputer: {}".format(comPilih))
+    print(gameIcon[comPilih])
+
+
+    if userPilih == 0 and comPilih == 2:
+        print("Kamu menang!")
+    elif comPilih == 0 and userPilih == 2:
+        print("Kamu kalah!")
+    elif comPilih > userPilih:
+        print("Kamu kalah!")
+    elif userPilih > comPilih:
+        print("Kamu menang!")
+    elif comPilih == comPilih:
+        print("Draw!")
